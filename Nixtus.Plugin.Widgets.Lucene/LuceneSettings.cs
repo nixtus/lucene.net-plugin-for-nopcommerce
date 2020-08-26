59f9149ae4efae197ae07879756bd7506b147e74
Mike <mewajda@gmail.com>
2020-08-25 21:36:47 -0500

    feat: enable autocomplete to use lucene
    https://github.com/nixtus/lucene.net-plugin-for-nopcommerce/issues/2

--- Nixtus.Plugin.Widgets.Lucene/LuceneSettings.cs
+++ Nixtus.Plugin.Widgets.Lucene/LuceneSettings.cs
@@ -5,5 +5,7 @@ namespace Nixtus.Plugin.Widgets.Lucene
     public class LuceneSettings : ISettings
     {
         public bool Enabled { get; set; }
+
+        public bool AutoCompleteSearchEnabled { get; set; }
     }
 }
