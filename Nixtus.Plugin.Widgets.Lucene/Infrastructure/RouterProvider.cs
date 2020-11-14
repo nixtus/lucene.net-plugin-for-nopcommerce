ba7ca9d321f6a82c7b94a6446366866ba57355de
Mike <mewajda@gmail.com>
2020-11-14 11:45:56 -0600

    fix(autocomplete): override auto complete route

--- Nixtus.Plugin.Widgets.Lucene/Infrastructure/RouterProvider.cs
+++ Nixtus.Plugin.Widgets.Lucene/Infrastructure/RouterProvider.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Routing;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Nop.Core.Data;
+using Nop.Core.Domain.Localization;
+using Nop.Services.Localization;
+using Nop.Web.Framework.Localization;
 using Nop.Web.Framework.Mvc.Routing;
 
 namespace Nixtus.Plugin.Widgets.Lucene.Infrastructure
@@ -9,7 +15,23 @@ namespace Nixtus.Plugin.Widgets.Lucene.Infrastructure
 
         public void RegisterRoutes(IRouteBuilder endpointRouteBuilder)
         {
-            
+            var pattern = string.Empty;
+            if (DataSettingsManager.DatabaseIsInstalled)
+            {
+                var localizationSettings = endpointRouteBuilder.ServiceProvider.GetRequiredService<LocalizationSettings>();
+                if (localizationSettings.SeoFriendlyUrlsForLanguagesEnabled)
+                {
+                    var langservice = endpointRouteBuilder.ServiceProvider.GetRequiredService<ILanguageService>();
+                    var languages = langservice.GetAllLanguages().ToList();
+                    pattern = "{language:lang=" + languages.FirstOrDefault().UniqueSeoCode + "}/";
+                }
+            }
+
+            // override autocomplete search
+            endpointRouteBuilder.MapLocalizedRoute(
+                "ProductSearchAutoCompleteLuceneOverride", 
+                $"{pattern}catalog/searchtermautocomplete",
+                new { controller = "LuceneCatalog", action = "SearchTermAutoComplete" });
         }
     }
 }
